16dc04ff1d112fdeadec5cf260f44bafd05b99ac
Jayeshc1 <jayeshchavan045@gmail.com>
2023-06-24 14:00:42 +0530

    Add division operartion in multiplication file

--- src/com/demo/Multiplication.java
+++ src/com/demo/Multiplication.java
@@ -4,9 +4,11 @@ public class Multiplication {
 
 	public static void main(String[] args) {
 		
-	int a = 10;
-	int b = 20;
+	int a = 100;
+	int b = 10;
 	
 	System.out.println(a*b);
+	System.out.println(a/b);
 }
+		
 }
